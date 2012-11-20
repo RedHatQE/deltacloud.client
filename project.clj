2d002e03c7dae245e901ad6b2814b415ae52da00
Jeff Weiss <jweiss@redhat.com>
2012-11-20 16:40:38 -0500

    Move code over from katello automation project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject com.redhat.qe/deltacloud.client "1.0.0-SNAPSHOT"
+  :description "A clojure client for deltacloud"  
+  :dependencies [[org.clojure/core.incubator "0.1.0"]
+                 [org.clojure/data.json "0.1.1"]
+                 [clj-http "0.5.5"]])
